9fcf3ecba20d25647b958d941b8a891eb357b1bc
Roland Dominik Szilagyi <szilagyi.roland.dominik@gmail.com>
2021-01-25 23:36:51 +0100

    implement LoginProcedure method

--- SaintSender.DesktopUI/Views/MainWindow.xaml.cs
+++ SaintSender.DesktopUI/Views/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using SaintSender.DesktopUI.ViewModels;
+﻿using System;
+using SaintSender.DesktopUI.ViewModels;
+using SaintSender.DesktopUI.Views;
 using System.Windows;
+using SaintSender.Core.Models;
 
 namespace SaintSender.DesktopUI
 {
@@ -9,13 +12,35 @@ namespace SaintSender.DesktopUI
     public partial class MainWindow : Window
     {
         private MainWindowViewModel _vm;
+        private Account _account;
 
         public MainWindow()
         {
             // set DataContext to the ViewModel object
             _vm = new MainWindowViewModel();
-            DataContext = _vm;
+            LoginProcedure();
             InitializeComponent();
+        }
+
+        private void LoginProcedure()
+        {
+            if (Account.SavedCredentialsFound())
+            {
+                Account account = Account.LoadCredentials();
+                if (!account.RememberUserCredentials)
+                {
+                    AskForLogin();
+                }
+            }
+            else
+            {
+                AskForLogin();
+            }
+        }
+
+        private void AskForLogin()
+        {
+            new LoginWindow().ShowDialog();
         }
 
         private void GreetBtn_Click(object sender, RoutedEventArgs e)
