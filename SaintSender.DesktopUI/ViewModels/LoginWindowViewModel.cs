a50f09d25394a4c0719e0e64398f285786b97b3e
Roland Dominik Szilagyi <szilagyi.roland.dominik@gmail.com>
2021-01-25 23:37:40 +0100

    added LoginWindowViewModel for LoginWindow class

---
+++ SaintSender.DesktopUI/ViewModels/LoginWindowViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+using SaintSender.Core.Models;
+
+namespace SaintSender.DesktopUI.ViewModels
+{
+    public class LoginWindowViewModel : Window
+    {
+        private Account _account;
+
+        public LoginWindowViewModel()
+        {
+            _account = new Account();
+        }
+    }
+}
