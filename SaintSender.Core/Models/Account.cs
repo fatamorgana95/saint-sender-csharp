2dd8269ebac836291572d7e3e29c52c3344f8bcc
Roland Dominik Szilagyi <szilagyi.roland.dominik@gmail.com>
2021-01-25 16:06:39 +0100

    create Account class, for storing user credentials

---
+++ SaintSender.Core/Models/Account.cs
@@ -0,0 +1,40 @@
+﻿using System.Security;
+
+namespace SaintSender.Core.Models
+{
+    public class Account
+    {
+        private string _username;
+        private SecureString _password;
+        private bool _rememberUserCredentials;
+
+        public Account()
+        {
+        }
+
+        public Account(string username, SecureString password, bool rememberUserCredentials)
+        {
+            _username = username;
+            _password = password;
+            _rememberUserCredentials = rememberUserCredentials;
+        }
+
+        public string Username
+        {
+            get => _username;
+            set => _username = value;
+        }
+
+        public SecureString Password
+        {
+            get => _password;
+            set => _password = value;
+        }
+
+        public bool RememberUserCredentials
+        {
+            get => _rememberUserCredentials;
+            set => _rememberUserCredentials = value;
+        }
+    }
+}
