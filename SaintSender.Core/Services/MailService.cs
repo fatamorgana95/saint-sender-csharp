47c2979847432a04dd203bf69558abad44a46411
vb <verabertalan@gmail.com>
2021-01-26 17:31:43 +0100

    refactored MailService

--- SaintSender.Core/Services/MailService.cs
+++ SaintSender.Core/Services/MailService.cs
@@ -9,9 +9,11 @@ namespace SaintSender.Core.Services
 {
     public class MailService
     {
+
+        private static List<Email> _emails = new List<Email>();
+
         public static List<Email> GetMails(string username, string password)
         {
-            List<Email> emails = new List<Email>();
             using (var client = new ImapClient())
             {
                 client.Connect("imap.gmail.com", 993, true);
@@ -19,23 +21,28 @@ namespace SaintSender.Core.Services
                 //The Inbox folder is always available on all IMAP servers...
                 IMailFolder inbox = client.Inbox;
                 inbox.Open(FolderAccess.ReadOnly);
-                var uniqueIdList = client.Inbox.Search(SearchQuery.All);
-
-                foreach (UniqueId id in uniqueIdList)
-                {
-                    var info = client.Inbox.Fetch(new[] { id }, MessageSummaryItems.Flags);
-                    var seen = info[0].Flags.Value.HasFlag(MessageFlags.Seen);
-                    var mail = client.Inbox.GetMessage(id);
-                    var sender = mail.Sender == null ? null : mail.Sender.ToString();
-
-                    Email email = new Email(seen, sender, mail.Subject, mail.Date.DateTime);
-                    emails.Add(email);
-                }
+                
+                AddEmailToList(client);
 
                 client.Disconnect(true);
             }
 
-            return emails;
+            return _emails;
+        }
+
+        private static void AddEmailToList(ImapClient client)
+        {
+            var uniqueIdList = client.Inbox.Search(SearchQuery.All);
+            foreach (UniqueId id in uniqueIdList)
+            {
+                var info = client.Inbox.Fetch(new[] {id}, MessageSummaryItems.Flags);
+                var seen = info[0].Flags.Value.HasFlag(MessageFlags.Seen);
+                var mail = client.Inbox.GetMessage(id);
+                var sender = mail.Sender == null ? null : mail.Sender.ToString();
+
+                Email email = new Email(seen, sender, mail.Subject, mail.Date.DateTime);
+                _emails.Add(email);
+            }
         }
     }
 }
