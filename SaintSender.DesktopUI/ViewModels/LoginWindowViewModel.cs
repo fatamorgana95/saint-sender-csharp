c2cd159ce67981cfbc8eb441e1a9200bb779b9a1
Roland Dominik Szilagyi <szilagyi.roland.dominik@gmail.com>
2021-01-26 14:55:51 +0100

    added WarningMessage for databinding

--- SaintSender.DesktopUI/ViewModels/LoginWindowViewModel.cs
+++ SaintSender.DesktopUI/ViewModels/LoginWindowViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System.ComponentModel;
 using System.Security;
 using SaintSender.Core.Models;
+using SaintSender.Core.Services;
 
 namespace SaintSender.DesktopUI.ViewModels
 {
@@ -9,7 +10,7 @@ namespace SaintSender.DesktopUI.ViewModels
         public event PropertyChangedEventHandler PropertyChanged;
         private SecureString _password;
         private string _username;
-
+        private string _warningMessage;
         private Account _account;
 
 
@@ -21,10 +22,7 @@ namespace SaintSender.DesktopUI.ViewModels
         public SecureString Password
         {
             get { return _password; }
-            set
-            {
-                _password = value;
-            }
+            set { _password = value; }
         }
 
         public string Username
@@ -35,6 +33,21 @@ namespace SaintSender.DesktopUI.ViewModels
                 _username = value;
                 PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Username)));
             }
+        }
+
+        public string WarningMessage
+        {
+            get { return _warningMessage; }
+            set
+            {
+                _warningMessage = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(WarningMessage)));
+            }
+        }
+
+        public bool AttemptLogin(string username, string password)
+        {
+            return MailService.IsCorrectLoginCredentials(username, password);
         }
     }
 }
