efb6bc125daab6cc7218dbdc0a1993678ae83756
Mate Szathmari <mateszathmari@gmail.com>
2021-01-26 09:28:16 +0100

    Data bingind stuck

--- SaintSender.DesktopUI/ViewModels/LoginWindowViewModel.cs
+++ SaintSender.DesktopUI/ViewModels/LoginWindowViewModel.cs
@@ -1,15 +1,44 @@
-﻿using System.Windows;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Security;
+using System.Windows;
 using SaintSender.Core.Models;
+using SaintSender.DesktopUI.Annotations;
 
 namespace SaintSender.DesktopUI.ViewModels
 {
-    public class LoginWindowViewModel : Window
+    public class LoginWindowViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private SecureString _password;
+        private string _username;
+
         private Account _account;
+
 
         public LoginWindowViewModel()
         {
             _account = new Account();
+        }
+
+        public SecureString Password
+        {
+            get { return _password; }
+            set
+            {
+                _password = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(PasswordTxtB)));
+            }
+        }
+
+        public string Username
+        {
+            get { return _username; }
+            set
+            {
+                _username = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(UsernameTxtB)));
+            }
         }
     }
 }
