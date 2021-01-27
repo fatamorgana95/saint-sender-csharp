7d73b138992ebc80d1eaa2b82fc00f7f4cb14d69
Mate Szathmari <mateszathmari@gmail.com>
2021-01-27 05:33:43 +0100

    mailService.cs send new email method

--- SaintSender.Core/Services/MailService.cs
+++ SaintSender.Core/Services/MailService.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using MailKit;
 using MailKit.Net.Imap;
+using MailKit.Net.Smtp;
 using MimeKit;
 
 namespace SaintSender.Core.Services
@@ -27,6 +28,34 @@ namespace SaintSender.Core.Services
             }
 
             return emails;
+        }
+
+        public static void SendNewEmail(string text, string toMail, string toName)
+        {
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("Joey Tribbiani", "joey@friends.com"));
+            message.To.Add(new MailboxAddress("Mrs. Chanandler Bong", "chandler@friends.com"));
+            message.Subject = "How you doin'?";
+
+            message.Body = new TextPart("plain")
+            {
+                Text = @"Hey Chandler,
+
+I just wanted to let you know that Monica and I were going to go play some paintball, you in?
+
+-- Joey"
+            };
+
+            using (var client = new SmtpClient())
+            {
+                client.Connect("smtp.friends.com", 587, false);
+
+                // Note: only needed if the SMTP server requires authentication
+                client.Authenticate("joey", "password");
+
+                client.Send(message);
+                client.Disconnect(true);
+            }
         }
 
         public static bool IsCorrectLoginCredentials(string username, string password)
