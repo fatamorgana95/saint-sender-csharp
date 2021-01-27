1db3cefdf2b284dee66992676b18cef4db48d1f6
vb <verabertalan@gmail.com>
2021-01-27 14:47:58 +0100

    read/unread colors working with IValueConverter

--- SaintSender.DesktopUI/BackgroundConverter.cs
+++ SaintSender.DesktopUI/BackgroundConverter.cs
@@ -2,13 +2,13 @@
 using System.Globalization;
 using System.Windows.Data;
 
-namespace SaintSender.DesktopUI.Views
+namespace SaintSender.DesktopUI
 {
     public class BackgroundConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            throw new NotImplementedException();
+            return (bool)value ? "AliceBlue" : "Bisque";
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
